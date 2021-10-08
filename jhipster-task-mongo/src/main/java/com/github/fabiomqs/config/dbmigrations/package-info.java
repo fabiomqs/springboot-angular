77428000794885c72994c88405ebfc0b6241af27
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-10-08 10:54:18 -0300

    remove project because zephyr template is nor working

--- jhipster-task-mongo/src/main/java/com/github/fabiomqs/config/dbmigrations/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * MongoDB database migrations using Mongock.
- */
-package com.github.fabiomqs.config.dbmigrations;
--- jhipster-task-mongo/src/main/java/com/github/fabiomqs/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.fabiomqs.repository;
-
-import com.github.fabiomqs.domain.Authority;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-/**
- * Spring Data MongoDB repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends MongoRepository<Authority, String> {}
