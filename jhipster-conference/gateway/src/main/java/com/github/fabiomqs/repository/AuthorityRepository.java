2f110699566c00bd0ce6a1a47e3db47038c09f85
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-10-09 05:09:47 -0300

    Initial version of gateway generated by generator-jhipster@7.2.0

---
+++ jhipster-conference/gateway/src/main/java/com/github/fabiomqs/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.github.fabiomqs.repository;
+
+import com.github.fabiomqs.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
